c2ecce0d1c740fa23a540cf0ed7890693f8f00da
cash <cash@DESKTOP-18KNVUE>
2025-05-20 17:12:03 +0200

    Add project files.

---
+++ TwelvvyRestaurantApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TwelvvyRestaurantApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TwelvvyRestaurantApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TwelvvyRestaurantApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
